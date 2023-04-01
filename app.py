125f59cfd03bb6bb02852351514faa51aeac8452
longfangsong <longfangsong@icloud.com>
2023-04-02 00:52:11 +0800

    send telegram message

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 from EdgeGPT import Chatbot
 import asyncio
+import os
+import requests
+
+
+def send_telegram_message(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {"chat_id": chat_id, "text": message}
+    response = requests.post(url, data=data)
+    return response.json()
+
+
+token = os.environ.get("TELEGRAM_TOKEN")
+chat_id = "1199598103"
 
 
 async def ask(question):
@@ -12,4 +25,5 @@ async def ask(question):
 if __name__ == '__main__':
     with open('question.txt', 'r') as f:
         question = f.read()
-    print(asyncio.run(ask(question)))
+    message = asyncio.run(ask(question))
+    send_telegram_message(token, chat_id, message)

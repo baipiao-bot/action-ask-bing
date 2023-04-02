5411216b91b4186ec3f16809c6759975e14e495c
longfangsong <longfangsong@icloud.com>
2023-04-02 11:12:26 +0800

    Use json format
    Ask
    
    Ask
    
    Ask
    
    Ask
    
    Ask
    
    Ask

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from http.server import BaseHTTPRequestHandler
+import json
 from EdgeGPT import Chatbot
 import asyncio
 import os
 import requests
 
+token = os.environ.get("TELEGRAM_TOKEN")
+
 
 def send_telegram_message(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
+    data = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
     response = requests.post(url, data=data)
     return response.json()
-
-
-token = os.environ.get("TELEGRAM_TOKEN")
-chat_id = "1199598103"
 
 
 async def ask(question):
     bot = Chatbot(cookiePath='./cookies.json')
     result = await bot.ask(question)
-    print(result['item']['messages'][1]['text'])
     return result['item']['messages'][1]['text']
 
 if __name__ == '__main__':
-    with open('question.txt', 'r') as f:
-        question = f.read()
+    with open('question.json', 'r') as f:
+        data = json.load(f)
+        question = data['question']
+        chat_id = data['chat_id']
+
     message = asyncio.run(ask(question))
     print(send_telegram_message(token, chat_id, message))

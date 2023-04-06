22544204dd763b45aca16bde94ffe4894199ede4
longfangsong <longfangsong@icloud.com>
2023-04-06 16:38:15 +0800

    typing status

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use edge_gpt::{ChatSession, ConversationStyle, CookieInFile};
+use edge_gpt::{ChatSession, ConversationStyle, CookieInFile, NewBingResponseMessage};
 use ezio::prelude::*;
 use isahc::AsyncReadResponseExt;
 use libaes::Cipher;
 use redis::AsyncCommands;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
-use std::env;
-
+use std::{env, mem, time::Duration};
+use tokio::{sync::broadcast, time::interval};
 fn decrypt(data: &[u8], secret: &[u8]) -> String {
     let key = &secret[0..32];
     let iv = &secret[32..(32 + 16)];
@@ -29,6 +30,7 @@ pub struct Response {
     reply_to_message_id: i64,
     text: String,
     parse_mode: String,
+    disable_web_page_preview: bool,
 }
 
 impl Default for Response {
@@ -38,16 +40,60 @@ impl Default for Response {
             reply_to_message_id: Default::default(),
             text: Default::default(),
             parse_mode: "Markdown".to_string(),
+            disable_web_page_preview: true,
         }
     }
 }
 
+fn sup(mut number: u8) -> String {
+    const SUP_CHARACTERS: &str = "⁰¹²³⁴⁵⁶⁷⁸⁹";
+    let mut result = String::new();
+    let hundred = number / 100;
+    if hundred > 0 {
+        result.push(SUP_CHARACTERS.chars().nth(hundred as usize).unwrap());
+    }
+    number %= 100;
+    let ten = number / 10;
+    if hundred > 0 || ten > 0 {
+        result.push(SUP_CHARACTERS.chars().nth(ten as usize).unwrap());
+    }
+    number %= 10;
+    result.push(SUP_CHARACTERS.chars().nth(number as usize).unwrap());
+    result
+}
+
 impl Response {
-    pub fn new(chat_id: i64, reply_to_message_id: i64, answer: String) -> Self {
+    fn fix_attributions(answer: &mut NewBingResponseMessage) {
+        let mut text = mem::take(&mut answer.text);
+        for (attribution_id, source_attribution) in answer.source_attributions.iter().enumerate() {
+            let display_form_attribution_id = attribution_id + 1;
+            let display_form_attribution_id_sup_str = sup(display_form_attribution_id as _);
+            text = text.replace(
+                &format!("[^{display_form_attribution_id}^]"),
+                &format!("[{display_form_attribution_id_sup_str}]({source_attribution})"),
+            );
+        }
+        answer.text = text;
+    }
+
+    fn fix_unordered_list(answer: &mut NewBingResponseMessage) {
+        answer.text.insert(0, '\n');
+        let re = Regex::new("^[-]").unwrap();
+        answer.text = re.replace(&answer.text, "•").to_string();
+        answer.text.remove(0);
+    }
+
+    fn parse_answer(mut answer: NewBingResponseMessage) -> String {
+        Self::fix_attributions(&mut answer);
+        Self::fix_unordered_list(&mut answer);
+        answer.text
+    }
+
+    pub fn new(chat_id: i64, reply_to_message_id: i64, answer: NewBingResponseMessage) -> Self {
         Self {
             chat_id,
             reply_to_message_id,
-            text: answer,
+            text: Self::parse_answer(answer),
             ..Default::default()
         }
     }
@@ -103,8 +149,32 @@ async fn main() {
         };
         ChatSession::create(style, &cookies).await
     };
+
+    let (stop_typing_action_tx, mut stop_typing_action_rx) = broadcast::channel(1);
+    let chat_id = request.chat_id;
+    let telegram_token_cloned = telegram_token.clone();
+    tokio::spawn(async move {
+        let mut interval = interval(Duration::from_secs(5));
+        loop {
+            tokio::select! {
+                _ = stop_typing_action_rx.recv() => {
+                    break;
+                }
+                _ = interval.tick() => {
+                    let send_message_request: isahc::Request<String> = isahc::Request::builder()
+                        .uri(format!("https://api.telegram.org/bot{telegram_token_cloned}/sendChatAction"))
+                        .header("Content-Type", "application/json")
+                        .method("POST")
+                        .body(format!("{{\"chat_id\": {chat_id}, \"action\": \"typing\"}}"))
+                        .unwrap();
+                    isahc::send_async(send_message_request).await.unwrap();
+                }
+            }
+        }
+    });
+
     let response = chat_session.send_message(&request.question).await.unwrap();
-    let telegram_response = Response::new(request.chat_id, request.message_id, response.text);
+    let telegram_response = Response::new(request.chat_id, request.message_id, response);
     let response_json = serde_json::to_string(&telegram_response).unwrap();
     let url = format!("https://api.telegram.org/bot{telegram_token}/sendMessage");
     let session_str = serde_json::to_string(&chat_session).unwrap();
@@ -115,6 +185,7 @@ async fn main() {
         .body(response_json)
         .unwrap();
     let mut send_message_response = isahc::send_async(send_message_request).await.unwrap();
+    stop_typing_action_tx.send(()).unwrap();
     let send_message_response: SendMessageResponse = send_message_response.json().await.unwrap();
     let key = format!(
         "{}-{}",
